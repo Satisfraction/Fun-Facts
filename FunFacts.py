1e83124a48a63b040697d4ec22da4b668a47f4b1
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2023-05-21 15:49:43 +0200

    Mayor Update
    switch from tkinter to kivy for possible future app support (experimental for now)
    add some style elements

--- FunFacts.py
+++ FunFacts.py
@@ -1,32 +1,39 @@
-import tkinter as tk
 import requests
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.config import Config
 
-class FunFacts:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("FunFacts")
-        
-        # Schaltfläche zum Anzeigen des Fakts
-        self.fact_button = tk.Button(root, text="Zeige mir einen FunFact!", command=self.show_fact)
-        self.fact_button.pack(pady=10)
-        
-        # Text-Widget für die Anzeige des FunFacts
-        self.fact_text = tk.Text(root, height=10, width=50, wrap="word")
-        self.fact_text.pack(pady=10)
-        
-    def show_fact(self):
-        # API-Aufruf, um einen zufälligen Fakt abzurufen
-        response = requests.get("https://uselessfacts.jsph.pl/random.json?language=de")
-        
-        # Ergebnis in einem Text-Widget anzeigen
+Config.set('graphics', 'width', '800')
+Config.set('graphics', 'height', '400')
+Config.set('graphics', 'resizable', 0)
+
+class FunFacts(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'vertical'
+        self.padding = 20
+        self.spacing = 20
+        self.background_color = (1, 0.5, 0.5, 1)
+        self.add_widget(Label(text='FunFacts', font_size=30, color=(1, 1, 1, 1)))
+        self.fact_button = Button(text='Zeige mir einen FunFact!', font_size=20, background_color=(1, 1, 1, 1), size_hint=(1, 0.5))
+        self.fact_button.bind(on_release=self.show_fact)
+        self.add_widget(self.fact_button)
+        self.fact_text = Label(text='', font_size=20, color=(1, 1, 1, 1))
+        self.add_widget(self.fact_text)
+
+    def show_fact(self, button):
+        response = requests.get('https://uselessfacts.jsph.pl/random.json?language=de')
         if response.status_code == 200:
-            fact = response.json()["text"]
-            self.fact_text.delete("1.0", "end")
-            self.fact_text.insert("end", fact)
+            fact = response.json()['text']
+            self.fact_text.text = fact
         else:
-            self.fact_text.delete("1.0", "end")
-            self.fact_text.insert("end", "Konnte keinen Fakt abrufen")
-        
-root = tk.Tk()
-fun_facts = FunFacts(root)
-root.mainloop()
+            self.fact_text.text = 'Konnte keinen Fakt abrufen'
+
+class FunFactsApp(App):
+    def build(self):
+        return FunFacts()
+
+if __name__ == '__main__':
+    FunFactsApp().run()
